946666303e1daf170e80c0e1bdd44f64cf9bc77e
Maxim Koltsov <kolmax94@gmail.com>
2020-04-09 16:59:20 +0300

    version 0.1.2.0: improve logging (#13)

--- src/Web/Template/Server.hs
+++ src/Web/Template/Server.hs
@@ -14,7 +14,9 @@ module Web.Template.Server
     ) where
 
 import           Control.Concurrent        (threadDelay)
-import           Control.Exception         (SomeException, catch)
+import           Control.Exception         (AsyncException (..),
+                                            SomeException (..), catch,
+                                            fromException)
 import           Control.Monad             (unless)
 import           Control.Monad.RWS         (RWST, evalRWST)
 import           Data.String               (fromString)
@@ -22,11 +24,13 @@ import           Data.Text.Encoding        (encodeUtf8)
 import           Data.Text.Lazy            as TL (Text, toStrict)
 import           Network.HTTP.Types.Header (Header)
 import           Network.HTTP.Types.Status (status401)
-import           Network.Wai               (Application, Middleware,
+import           Network.Wai               (Application, Middleware, Request,
                                             mapResponseHeaders, modifyResponse)
 import           Network.Wai.Handler.Warp  (defaultSettings,
                                             exceptionResponseForDebug,
+                                            setOnException,
                                             setOnExceptionResponse, setPort)
+import           System.BCD.Log            (error')
 import           Web.Cookie                (parseCookiesText)
 import           Web.Scotty.Trans          (Options (..), ScottyT,
                                             defaultHandler, header, json,
@@ -45,11 +49,15 @@ restartOnError :: IO () -> Int -> IO ()
 restartOnError f delayUs = f `catch` handle
   where
     handle :: SomeException -> IO ()
-    handle e = do
-        putStrLn $ "unexpected exception\n" ++ show e
-        putStrLn $ "server will be restarted in " ++ show delayUs ++ "us"
-        threadDelay delayUs
-        restartOnError f delayUs
+    handle e =
+      case fromException e of
+        -- Program should be killable by Ctrl-C.
+        Just UserInterrupt -> return ()
+        _ -> do
+            putStrLn $ "unexpected exception\n" <> show e
+            putStrLn $ "server will be restarted in " <> show delayUs <> "us"
+            threadDelay delayUs
+            restartOnError f delayUs
 
 -- | For given port and server settings run the server.
 runWebServer :: (Monoid w, Show w) => Port -> CustomWebServer r w s -> IO ()
@@ -79,12 +87,19 @@ defaultHeaderCORS = modifyResponse (mapResponseHeaders addHeaderCORS)
         Nothing -> ("Access-Control-Allow-Origin", "*") : headers
 
 runRoute :: Monoid w => Route r w s -> ScottyM r w s ()
-runRoute Route {..} = method (fromString $ "/:version" ++ path) (checkVersion version . auth $ process)
+runRoute Route {..} = method (fromString $ "/:version" <> path) (checkVersion version . auth $ process)
 
 scottyOpts :: Port -> Options
 scottyOpts port = Options 1 warpSettings
   where
-    warpSettings = setOnExceptionResponse exceptionResponseForDebug . setPort port $ defaultSettings
+    warpSettings =
+      setOnException onException
+      . setOnExceptionResponse exceptionResponseForDebug
+      . setPort port
+      $ defaultSettings
+
+onException :: Maybe Request -> SomeException -> IO ()
+onException _ e = error' ("scotty" :: Text) $ show e
 
 auth :: Monoid w => Process r w s -> WebM r w s ()
 auth (Process p) = p
@@ -100,7 +115,7 @@ auth (AuthProcess p) = do
 checkVersion :: Monoid w => Int -> WebM r w s () -> WebM r w s ()
 checkVersion version route = do
     versionPath <- param "version"
-    unless ("v" ++ show version == versionPath) next
+    unless ("v" <> show version == versionPath) next
     route
 
 getIdFromCookies :: TL.Text -> Maybe UserId
