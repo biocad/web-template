2005ddbdf353e0294e8873eaccad890d23f75863
Maxim Koltsov <kolmax94@gmail.com>
2020-06-05 17:12:08 +0300

    CBDINFRA-84 Initial servant support
    CBDINFRA-92 DerivingVia ToJSON + FromJSON + ToSchema
    CBDINFRA-96 Examples in Swagger

---
+++ src/Web/Template/Servant.hs
@@ -0,0 +1,1 @@
+module Web.Template.Servant where
---
+++ src/Web/Template/Servant/API.hs
@@ -0,0 +1,34 @@
+module Web.Template.Servant.API where
+
+import Control.Lens  ((?~))
+import Data.Function ((&))
+import Data.Proxy    (Proxy (..))
+import Data.String   (fromString)
+import Data.Swagger  (applyTags, description)
+import Data.Text     (pack)
+import GHC.TypeLits  (AppendSymbol, KnownSymbol, Symbol, symbolVal)
+
+import Servant         ((:>), HasServer (..))
+import Servant.Swagger (HasSwagger (..))
+
+-- | Prepend version to every sub-route.
+--
+-- > type API = Version "3" :> ("route1" :<|> "route2")
+type Version (v :: Symbol) = AppendSymbol "v" v
+
+-- | Mark sub-api with a Swagger tag with description.
+--
+-- > type API
+-- >   =    (Tag "foo" "Some Foo routes" :> ("foo1" :<|> "foo2"))
+-- >   :<|> (Tag "bar" "Some Bar routes" :> ("bar1" :<|> "bar2"))
+data Tag (tag :: Symbol) (descr :: Symbol)
+
+instance HasServer api context => HasServer (Tag tag descr :> api) context where
+  type ServerT (Tag tag descr :> api) m = ServerT api m
+
+  route _ = route @api Proxy
+  hoistServerWithContext _ = hoistServerWithContext @api Proxy
+
+instance (KnownSymbol tag, KnownSymbol descr, HasSwagger api) => HasSwagger (Tag tag descr :> api) where
+  toSwagger _ = toSwagger @api Proxy
+    & applyTags [fromString (symbolVal @tag Proxy) & description ?~ pack (symbolVal @descr Proxy)]
